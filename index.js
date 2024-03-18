adc78cbfcd849a9b4c619a0fc7c20e912f4314ad
GeorgeKahugu <gee.kahugu@gmail.com>
2024-03-18 21:04:28 +0300

    review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'George Kahugu';
+
+const welcomeMessage ='Welcome to Flatbook, ${George Kahugu}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, G!'
+
+
+
+
+
+
+
+
+
